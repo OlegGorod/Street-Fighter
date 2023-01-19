7e2d342b9a149da8423fe5e34273b97574b69130
OlegGorodenskyi <olgo@owort.com>
2023-01-19 15:12:56 +0200

    Place fighters into page

---
+++ src/javascript/components/fighterPreview.js
@@ -0,0 +1,36 @@
+import { createElement } from '../helpers/domHelper';
+
+export function createFighterPreview(fighter, position) {
+  const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+  const fighterElement = createElement({
+    tagName: 'div',
+    className: `fighter-preview___root ${positionClassName}`,
+  });
+
+  // todo: show fighter info (image, name, health, etc.)
+  const fragment = document.createDocumentFragment();
+  const name = fragment.appendChild(document.createElement('p'));
+  const health = fragment.appendChild(document.createElement('p'));
+  
+  name.textContent = fighter.name;
+  health.textContent = fighter.health;
+  fighterElement.appendChild(fragment);
+
+  return fighterElement;
+}
+
+export function createFighterImage(fighter) {
+  const { source, name } = fighter;
+  const attributes = { 
+    src: source, 
+    title: name,
+    alt: name 
+  };
+  const imgElement = createElement({
+    tagName: 'img',
+    className: 'fighter-preview___img',
+    attributes,
+  });
+
+  return imgElement;
+}
